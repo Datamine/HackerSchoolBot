4f197f113b63289c6dabf5e6bbf34ae682debe80
datamine <datamine@users.noreply.github.com>
2014-11-07 21:55:10 -0600

    up

--- twitterbot.py
+++ twitterbot.py
@@ -3,4 +3,14 @@
 from twitterapi import api
 import time
 
-hackerschoolers = ["johnloeber","hackerschool"]
+def main():
+    with open("hackerschoolers2","r") as f:
+        hsers = map(rstrip('\n'), f.readlines())
+    while True:
+        recent = 
+ #       for i in hsers:
+  #          tweets = getTweet
+        sleep(120)
+
+if __name__=='__main__':
+    main()
