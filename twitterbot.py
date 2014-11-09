6cd70202bc49343908a3f496335a9170bf6c62a0
datamine <datamine@users.noreply.github.com>
2014-11-08 21:24:15 -0600

    some upds

--- twitterbot.py
+++ twitterbot.py
@@ -2,14 +2,26 @@
 
 from twitterapi import api
 import time
+from ast import literal_eval
 
 def main():
     with open("hackerschoolers2","r") as f:
         hsers = map(rstrip('\n'), f.readlines())
+    with open("hstimestamps","r+") as g:
+
     while True:
-        recent = 
- #       for i in hsers:
-  #          tweets = getTweet
+        for i in hsers:
+                # This is an inefficient way of maintaining a store of latest
+                # ids, but all other ways each only had one of (a) practicality,
+                # (b) data sanity in case of crashes, (c) efficiency.
+                # As the file in question is small, I'm going for an
+                # inefficient approach, as it's not so important.
+                
+                tweets = api.GetUserTimeline(screen_name=i,include_rts=False,
+                                exclude_replies=False,since_id=getsinceid(i,g))
+                for j in tweets:
+                     
+
         sleep(120)
 
 if __name__=='__main__':
